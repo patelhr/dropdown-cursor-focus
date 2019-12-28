8070046f593987ee61a0815fe5ddf056a0628fb3
hardik.patel@promactinfo.com <hardik.patel@promactinfo.com>
2019-12-28 11:51:07 +0530

    Added log

--- src/app/select-overview-example.ts
+++ src/app/select-overview-example.ts
@@ -1,4 +1,4 @@
-import {Component,ViewChild} from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 export interface Food {
   value: string;
@@ -14,25 +14,26 @@ export interface Food {
   styleUrls: ['select-overview-example.css'],
 })
 export class SelectOverviewExample {
-  @ViewChild("myTextArea") myTxtarea:any;
+  @ViewChild("myTextArea") myTxtarea: any;
   foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'}
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' }
   ];
-   caretPos: number = 0;
+  caretPos: number = 0;
 
-   callSomeFunction(){
-    setTimeout(()=>{
-     this.myTxtarea =document.getElementById("1");
-    this.myTxtarea.focus();
-    this.myTxtarea.selectionEnd= this.caretPos;
-    },0);
-   }
+  callSomeFunction() {
+    console.log('1');
+    setTimeout(() => {
+      this.myTxtarea = document.getElementById("1");
+      this.myTxtarea.focus();
+      this.myTxtarea.selectionEnd = this.caretPos;
+    }, 0);
+  }
 
   getCaretPos(oField) {
     if (oField.selectionStart || oField.selectionStart == '0') {
-       this.caretPos = oField.selectionStart;
+      this.caretPos = oField.selectionStart;
     }
   }
 }
